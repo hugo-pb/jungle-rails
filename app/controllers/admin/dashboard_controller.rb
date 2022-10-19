be6b7a3b7d2e08140ef984e01f5c98644bec3a84
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-19 16:57:31 -0600

    set up query and send products

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
    http_basic_authenticate_with name: ENV["NAME"], password: ENV["PASSWORD"]
+
   def show
+@count_products = Product.count
+@count_categories = Category.count
+  #fetch and pass data to template
   end
 end
