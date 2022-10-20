1f4e2335c7fdd8e0eb7754b1cc8761a04af73ecb
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-20 13:21:17 -0600

    index, new, create fror category controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,21 @@
 class Admin::CategoriesController < ApplicationController
 
+  def index
+    @categories = category.order(id: :desc).all
+  end
 
-  
+  def new
+    @category = category.new
+  end
+
+  def create
+    @category = category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'category created!'
+    else
+      render :new
+    end
+  end
+
 end
