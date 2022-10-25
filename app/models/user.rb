e87317c4a8afb894e2a378d6fa933346c65a66d2
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-24 22:56:12 -0600

    user log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,15 @@ class User < ApplicationRecord
      validates :password_confirmation, presence: true
      validates :email, :uniqueness => true
      validates :email, :uniqueness =>{case_sensitive: false}
-   validates :password, :length => { :minimum => 3 }
+     validates :password, :length => { :minimum => 3 }
      validates :name, presence: true
-
+     def self.authenticate_with_credentials(email, pass)
+       user = User.find_by_email(email)
+       p user
+       if user && user.authenticate(pass)
+        return user
+       else
+        return nil
+     end
     end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,10 +35,23 @@ RSpec.describe User, type: :model do
       @user.save
       expect(@user).to be_invalid
     end 
-    it 'password needs to be longer than 3' do
+    it 'validation without name' do
       @user = User.new(name: nil, email: 'TEST@TEST.com', password: "12", password_confirmation: "12")
       @user.save
       expect(@user).to be_invalid
     end    
+  end
+   describe '.authenticate_with_credentials' do
+    before do
+      @user = User.create(name: 'test', email: 'TEST@TEST.com', password: "1234", password_confirmation: "1234")
+    end
+   it 'It must be created with a password and password_confirmation fields' do
+     
+      
+      user_testing = User.authenticate_with_credentials('TEST@TEST.com', '1234')
+      p user_testing
+      expect(user_testing).to be_present
+
+    end
   end
 end
