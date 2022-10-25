b3a36819a5296f9bb50b78f39e21e628a8989006
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-25 15:39:42 -0600

    add mock data for test

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -2,4 +2,7 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
 });
