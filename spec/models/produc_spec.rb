90af73a26279678ea3c702cbb4a3cda508074165
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-24 18:40:12 -0600

    pass dummy test

--- spec/models/produc_spec.rb
+++ spec/models/produc_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe ProducSpec, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Product, type: :model do
+ describe 'Validations' do
+    context 'Creating new product' do
+      it "validates for a name" do
+        @product = Product.new
+        expect(@product.id).to be_nil
+      end
+    end
+  end
 end
