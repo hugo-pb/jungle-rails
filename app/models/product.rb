6e7fb7cdb2a0110d64e5c23d59e8058d455dc7e3
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-24 19:15:08 -0600

    tests for validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,5 +7,5 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
-  validates :category, presence: true
+  validates :category_id, presence: true
 end
--- spec/models/produc_spec.rb
+++ spec/models/produc_spec.rb
@@ -3,9 +3,65 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
  describe 'Validations' do
     context 'Creating new product' do
-      it "validates for a name" do
-        @product = Product.new
-        expect(@product.id).to be_nil
+      it "validates for name" do
+        @category = Category.new(name: "trees")
+        @category.save
+
+        @product = Product.new(name:'tree life', 
+          quantity: 0,
+          price: 64.99,
+          category_id: @category.id
+        )
+        @product.save!
+        expect(@product.name).to be_present
+      end
+      it "validates for price" do
+        @category = Category.new(name: "trees")
+        @category.save
+
+        @product = Product.new(name:'tree life', 
+          quantity: 0,
+          price: 64.99,
+          category_id: @category.id
+        )
+        @product.save!
+        expect(@product.price).to be_present
+      end
+      it "validates for quantity" do
+        @category = Category.new(name: "trees")
+        @category.save
+
+        @product = Product.new(name:'tree life', 
+          quantity: 0,
+          price: 64.99,
+          category_id: @category.id
+        )
+        @product.save!
+        expect(@product.quantity).to be_present
+      end
+      it "validates for category" do
+        @category = Category.new(name: "trees")
+        @category.save
+
+        @product = Product.new(name:'tree life', 
+          quantity: 0,
+          price: 64.99,
+          category_id: @category.id
+        )
+        @product.save!
+        expect(@product.category).to be_present
+      end
+      it "Error when missing something" do
+        @category = Category.new(name: "trees")
+        @category.save
+
+        @product = Product.new(name:'tree life', 
+          quantity: nil,
+          price: 64.99,
+          category_id: @category.id
+        )
+        @product.save
+        expect(@product.errors.full_messages ).to be_present 
       end
     end
   end
