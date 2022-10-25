477c90b1325178c7116a141b3db6e49b167bcb2a
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-25 15:40:18 -0600

    add test to see if all products are on screen

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -5,4 +5,7 @@ describe("Navigation", () => {
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
 });
